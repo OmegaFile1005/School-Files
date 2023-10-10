b0ba52adab406c512c99cc0e7aef985d004377c0
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-10 13:09:00 -0400

    s

---
+++ Portfolio/js-type work/js expression/jscond1.js
@@ -0,0 +1,22 @@
+var number = parseFloat(prompt("Enter a number:"));
+
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+function checkNumber() {
+    var number = parseFloat(document.getElementById("number").value);
+    if (number > 0) {
+        alert("The number is positive.");
+    } else if (number < 0) {
+        alert("The number is negative.");
+    } else {
+        alert("The number is zero.");
+    }
+}
+
+document.getElementById("btn").addEventListener("click", checkNumber);
