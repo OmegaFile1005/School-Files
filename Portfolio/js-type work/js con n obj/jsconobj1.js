6f45351d6e1f0e973f2ad94478390a709844dc8b
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-12 08:39:31 -0400

    2

--- Portfolio/js-type work/js con n obj/jsconobj1.js
+++ Portfolio/js-type work/js con n obj/jsconobj1.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Check if the username and password are correct
+    if (username === "your_user" && password === "your_password") {
+        alert("Welcome, " + username + "!");
+    } else {
+        alert("Please register first.");
+    }
+}
+
+function register() {
+    // Open the registration page
+    window.location.href = "jscondobj1p2.html";
+}
