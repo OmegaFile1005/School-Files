6646007c2a0da375d00a2e6bb04abb7e8b1d6dc0
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-12 12:39:41 -0400

    a

--- Portfolio/js-type work/js con n obj/jsconobj1.js
+++ Portfolio/js-type work/js con n obj/jsconobj1.js
@@ -12,5 +12,42 @@ function login() {
 
 function register() {
     // Open the registration page
-    window.location.href = "jscondobj1p2.html";
+    window.location.href = "jsconobj1p2.html";
 }
+
+const userObj = {}; // Define userObj as a global variable
+
+function registerUser() {
+
+    const getUserInput = (id) => document.getElementById(id).value;
+
+    const name = getUserInput('name');
+    const lastName = getUserInput('lastName');
+    const address = getUserInput('address');
+    const age = getUserInput('age');
+    const gender = getUserInput('gender');
+    const user = getUserInput('user');
+    const password = getUserInput('password');
+
+    userObj.name = name;
+    userObj.lastName = lastName;
+    userObj.address = address;
+    userObj.age = age;
+    userObj.gender = gender;
+    userObj.user = user;
+    userObj.password = password;
+
+alert('User successfully registered!');}
+
+function getUserInfo() {
+    const userInfoDiv = document.getElementById('userInfo');
+    userInfoDiv.innerHTML = '';
+
+    for (const property in userObj) {
+        const p = document.createElement('p');
+        p.textContent = `${property}: ${userObj[property]}`;
+        userInfoDiv.appendChild(p);
+    }
+}
+
+// document.getElementById('register').addEventListener('click', register);
