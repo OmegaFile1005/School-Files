b02d24a14ed4f76703d9341e075b1b5d81441853
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-13 08:43:40 -0400

    c

--- Portfolio/js-type work/js con n obj/jsconobj3.js
+++ Portfolio/js-type work/js con n obj/jsconobj3.js
@@ -1,25 +1,20 @@
 function checkGuess() {
-    const guess = parseInt(document.getElementById('number').value);
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
-
-    if (isNaN(guess) || guess < 1 || guess > 10) {
-        alert("Please enter a valid number between 1 and 10.");
-    } else if (guess === randomNumber) {
-        alert("Congratulations! You guessed the correct number!");
-    } else {
-        alert("Sorry, your guess is incorrect. The correct number was: " + randomNumber);
-    }
-}
-
-function correctInput() {
     const input = document.getElementById('number');
     let value = parseInt(input.value);
 
-    if (isNaN(value) || value < 1) {
-        value = 1;
-    } else if (value > 10) {
-        value = 10;
+    if (isNaN(value) || value < 1 || value > 10) {
+        alert("Please enter a valid number between 1 and 10.");
+        return;
     }
 
-    input.value = value;
+    const guess = value;
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+    if (guess === randomNumber) {
+        alert("Congratulations! You guessed the correct number!");
+    } else if (guess >= 1 && guess <= 10) {
+        alert("The correct number was: " + randomNumber);
+    } else {
+        alert("Invalid input. Please enter a number between 1 and 10.");
+    }
 }
