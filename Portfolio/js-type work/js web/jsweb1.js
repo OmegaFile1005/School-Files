b5d2be165f91b4d39f96be7d3159a1d534be67a3
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-19 10:29:36 -0400

    1

--- Portfolio/js-type work/js web/jsweb1.js
+++ Portfolio/js-type work/js web/jsweb1.js
@@ -1,50 +1,76 @@
 function sendOrder() {
-    const size = document.querySelector('input[name="size"]:checked');
-    const toppings = Array.from(document.querySelectorAll('input[name="toppings"]:checked')).map(topping => topping.value);
+    const selectedSize = document.querySelector('input[name="size"]:checked').value;
+    const selectedToppings = Array.from(document.querySelectorAll('input[name="toppings"]:checked')).map(topping => topping.value);
 
-    const drinks = Array.from(document.querySelectorAll('.drink')).reduce((acc, drink) => {
-        const name = drink.querySelector('.drink-name').textContent;
-        const quantity = parseInt(drink.querySelector('.drink-quantity').value);
-        const price = parseFloat(drink.querySelector('.drink-price').textContent);
+    const selectedDrinks = Array.from(document.querySelectorAll('#drinks input[type="number"]')).reduce((acc, drink) => {
+        const name = drink.previousElementSibling.textContent;
+        const quantity = parseInt(drink.value) || 0;
+        const price = parseFloat(drink.getAttribute('data-price')) || 0;
         acc.push({ name, quantity, price });
         return acc;
     }, []);
 
     let totalCost = 0;
 
-    if (size === 'small') {
-        totalCost += 6.00;
-    } else if (size === 'medium') {
-        totalCost += 8.00;
-    } else if (size === 'large') {
-        totalCost += 10.00;
-    } else if (size === 'extra-large') {
-        totalCost += 12.00;
+    switch (selectedSize) {
+        case 'small':
+            totalCost += 6.00;
+            break;
+        case 'medium':
+            totalCost += 8.00;
+            break;
+        case 'large':
+            totalCost += 10.00;
+            break;
+        case 'extra-large':
+            totalCost += 12.00;
+            break;
     }
 
-    toppings.forEach(topping => {
-        if (topping === 'pepperoni' || topping === 'sausage') {
-            totalCost += 0.50;
-        } else if (topping === 'extra-cheese' || topping === 'olives' || topping === 'onions') {
-            totalCost += 0.25;
+    selectedToppings.forEach(topping => {
+        switch (topping) {
+            case 'pepperoni':
+            case 'sausage':
+                totalCost += 0.50;
+                break;
+            case 'extra-cheese':
+            case 'olives':
+            case 'onions':
+                totalCost += 0.25;
+                break;
         }
     });
 
-    const selectedDrink = document.querySelector('input[name="drink"]:checked');
-    if (selectedDrink === 'soda-can') {
-        totalCost += 1.50;
-    } else if (selectedDrink === 'soda-20oz') {
-        totalCost += 2.00;
-    } else if (selectedDrink === 'soda-2L') {
-        totalCost += 5.00;
-    } else if (selectedDrink === 'orange-juice') {
-        totalCost += 2.50;
-    } else if (selectedDrink === 'water') {
-        totalCost += 1.00;
-    }
+    selectedDrinks.forEach(_drink => {
+        const selectedDrink = document.querySelector('input[name="drink"]:checked').value;
 
-    localStorage.setItem('order', JSON.stringify({ size, toppings, drinks, totalCost }));
+        switch (selectedDrink) {
+            case 'soda-can':
+                totalCost += 1.50;
+                break;
+            case 'soda-20oz':
+                totalCost += 2.00;
+                break;
+            case 'soda-2L':
+                totalCost += 5.00;
+                break;
+            case 'orange-juice':
+                totalCost += 2.50;
+                break;
+            case 'water':
+                totalCost += 1.00;
+                break;
+        }
+    });
+
+    const receipt = `
+    Size: ${selectedSize}
+    Toppings: ${selectedToppings.join(', ')}
+    Drinks: ${selectedDrinks.map(drink => `${drink.name} x ${drink.quantity}`).join(', ')}
+    Total Cost: $${totalCost.toFixed(2)}
+  `;
+
+    document.getElementById('receipt').value = receipt;
+    localStorage.setItem('order', JSON.stringify({ size: selectedSize, toppings: selectedToppings, drinks: selectedDrinks, totalCost }));
     document.getElementById('total-cost').textContent = totalCost.toFixed(2);
-
-    console.log('Order sent:', { size, toppings, drinks, totalCost });
 }
