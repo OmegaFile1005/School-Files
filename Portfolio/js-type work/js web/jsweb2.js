7a02be1efa042dbe802daa85eae54b74931f5925
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-20 13:19:28 -0400

    finished

--- Portfolio/js-type work/js web/jsweb2.js
+++ Portfolio/js-type work/js web/jsweb2.js
@@ -1,45 +1,65 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var name = prompt("Please enter your name, last name, email, and phone number (separated by commas):");
-    
-    // Split the entered values using commas as the delimiter
-    var values = name.split(",");
-    var enteredName = values[0].trim();
-    var lastName = values[1].trim();
-    var email = values[2].trim();
-    var phoneNumber = values[3].trim();
+    var hasEnteredValues = localStorage.getItem("hasEnteredValues");
 
-    // Store the entered values in localStorage
-    localStorage.setItem("name", enteredName);
-    localStorage.setItem("lastName", lastName);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phoneNumber", phoneNumber);
+    if (!hasEnteredValues) {
+        var name = prompt("Please enter your name:");
+        var lastName = prompt("Please enter your last name:");
+        var email = prompt("Please enter your email:");
+        var phoneNumber = prompt("Please enter your phone number:");
+
+        // Store the entered values in localStorage
+        localStorage.setItem("name", name);
+        localStorage.setItem("lastName", lastName);
+        localStorage.setItem("email", email);
+        localStorage.setItem("phoneNumber", phoneNumber);
+
+        // Set the flag to indicate that values have been entered
+        localStorage.setItem("hasEnteredValues", true);
+    }
 });
 
 function sendTicket() {
-  const selectedClass = document.querySelector('input[name="class"]:checked')?.value;
-  const selectedType = document.querySelector('input[name="type"]:checked')?.value;
-  const selectedAge = Array.from(document.querySelectorAll('input[name="age"]:checked')).map(element => element.value);
-  const selectedDeparture = document.getElementById('departure')?.value;
-  const selectedDestination = document.getElementById('destination')?.value;
-  const selectedDate = document.getElementById('date')?.value;
+    const selectedClass = document.querySelector('input[name="class"]:checked')?.value;
+    const selectedType = document.querySelector('input[name="type"]:checked')?.value;
+    const selectedAge = Array.from(document.querySelectorAll('input[name="age"]:checked')).map(element => element.value);
+    const selectedDeparture = document.getElementById('departure')?.value;
+    const selectedDestination = document.getElementById('destination')?.value;
+    const selectedDate = document.getElementById('date')?.value;
 
-  if (!selectedClass || !selectedType || selectedAge.length === 0 || !selectedDeparture || !selectedDestination || !selectedDate) {
-    alert('Please select all fields.');
-    return;
-  }
+    if (!selectedClass || !selectedType || selectedAge.length === 0 || !selectedDeparture || !selectedDestination || !selectedDate) {
+        alert('Please select all fields.');
+        return;
+    }
 
-  const ticketInfo = {
-    class: selectedClass,
-    type: selectedType,
-    age: selectedAge,
-    departure: selectedDeparture,
-    destination: selectedDestination,
-    date: selectedDate
-  };
-  localStorage.setItem('ticket', JSON.stringify(ticketInfo));
+    const ticketInfo = {
+        class: selectedClass,
+        type: selectedType,
+        age: selectedAge,
+        departure: selectedDeparture,
+        destination: selectedDestination,
+        date: selectedDate
+    };
+    localStorage.setItem('ticket', JSON.stringify(ticketInfo));
 
-  window.location.href = 'jsweb2p2.html';
+    window.location.href = 'jsweb2p2.html';
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve stored ticket information from localStorage
+    const ticketInfo = JSON.parse(localStorage.getItem('ticket'));
+
+    // Display stored ticket information in the respective <p> elements
+    document.getElementById('class').textContent = ticketInfo.class;
+    document.getElementById('type').textContent = ticketInfo.type;
+    document.getElementById('age').textContent = ticketInfo.age.join(', ');
+    document.getElementById('departure').textContent = ticketInfo.departure;
+    document.getElementById('destination').textContent = ticketInfo.destination;
+    document.getElementById('date').textContent = ticketInfo.date;
+
+    // Clear the stored ticket information from localStorage
+    localStorage.removeItem('ticket');
+});
+
 function returnPage() {
     window.location.href = 'jsweb2p1.html';
 }
