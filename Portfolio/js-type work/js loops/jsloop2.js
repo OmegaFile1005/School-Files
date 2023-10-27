cc026b911c953ed0ae99c672abc7a9dec24880f6
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-27 13:39:18 -0400

    w

--- Portfolio/js-type work/js loops/jsloop2.js
+++ Portfolio/js-type work/js loops/jsloop2.js
@@ -1,51 +1,20 @@
-// function simulateGrowth() {
-//     var baseNumber = parseInt(document.getElementById("baseNumber").value);
-//     var growthRate = parseInt(document.getElementById("growthRate").value) / 100;
-//     var dayDeadline = parseInt(document.getElementById("dayDeadline").value);
-//     var textarea = document.getElementById("textarea");
-
-//     if (baseNumber <= 0 || growthRate <= 0 || dayDeadline <= 0) {
-//         alert("Please enter positive values for base number, growth rate, and day deadline.");
-//         return;
-//     }
-
-//     if (growthRate > 1) {
-//         alert("Growth rate must be less than or equal to 1.");
-//         return;
-//     }
-
-//     if (dayDeadline > 365) {
-//         alert("Day deadline must be less than or equal to 365.");
-//         return;
-//     }
-
-//     // textarea.value = "Day\tPopulation\n";
-
-//     for (var day = 1; day <= dayDeadline; day++) {
-//         var population = baseNumber * Math.pow(1 + growthRate, day);
-//         textarea.value += day + "\t" + population.toFixed(2) + "\n";
-
-//     }
-// }
-
 function simulateGrowth() {
-  var baseNumberInput = document.getElementById("baseNumber");
-  var growthRateInput = document.getElementById("growthRate");
-  var dayDeadlineInput = document.getElementById("dayDeadline");
+  const baseNumberInput = document.getElementById("baseNumber");
+  const growthRateInput = document.getElementById("growthRate");
+  const dayDeadlineInput = document.getElementById("dayDeadline");
 
-  if (!baseNumberInput || !growthRateInput || !dayDeadlineInput) {
-    console.error("One or more input elements not found.");
-    return;
+  const baseNumber = baseNumberInput.value;
+  const growthRate = ((growthRateInput.value / 100) + 1);
+  const dayDeadline = dayDeadlineInput.value;
+
+  let result = ""; // Create a variable to store the table of data
+
+  for (let i = 0; i < dayDeadline; i++) {
+    if (dayDeadline >= dayDeadline + 2) { break; }
+    const finalConversion = growthRate ** (i + 1);
+    const growthProduct = finalConversion * parseInt(baseNumber);
+    result += `Day: ${i + 1} | Final Count: ${growthProduct}\n`; // Append each row of data to the result variable
   }
 
-  var baseNumber = parseInt(baseNumberInput.value);
-  var growthRate = parseInt(growthRateInput.value) / 100;
-  var dayDeadline = parseInt(dayDeadlineInput.value);
-
-  textarea.value = "Day\tPopulation\n";
-
-  for (var day = 1; day <= dayDeadline; day++) {
-    var population = baseNumber * Math.pow(1 + growthRate, day);
-    textarea.value += day + "\t" + population.toFixed(2) + "\n";
-  }
+  document.getElementById("result").value = result; // Set the value of the textarea to the result variable
 }
--- Portfolio/js-type work/js loops/jsloop3.js
+++ Portfolio/js-type work/js loops/jsloop3.js
@@ -1,12 +1,12 @@
 // Susan is hired for a job, and her employer agrees to pay her every day.Her employer also agrees that Susan’s salary is 1 penny on the first day, 2 pennies on the second day, and 4 pennies on the third day, continuing to double each day.Create a program that allows the user to enter the number of days that Susan will work and calculates the total amount of pay she will receive over that period of time.
-
+// the value must start as 1 then multiply by 2 and add the previous value.
 function calculatePay() {
-    var baseNumberInput = document.getElementById("baseNumber");
-    var growthRateInput = document.getElementById("growthRate");
-    var dayDeadlineInput = document.getElementById("dayDeadline");
+    let dayLimit = parseInt(document.getElementById("days").value) * 1;
+    let startingPay = 1;
 
-    for (var day = 1; day <= dayDeadlineInput; day++) {
-        var payment = baseNumberInput * Math.pow(1 + growthRateInput, day);
-        document.getElementById("output").innerHTML += day + "\t" + payment.toFixed(2) + "\n";
+    for (let i = 0; i < dayLimit; i++) {
+        finalPay = startingPay * (2 ** dayLimit);
+        console.log(finalPay);
+        document.getElementById("result").value = finalPay;
     }
 }
