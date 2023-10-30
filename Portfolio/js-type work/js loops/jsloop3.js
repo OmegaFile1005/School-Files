c75adba56a83bccc62639518b7c2c7aa00e6f5ad
YongBo <54255241+robert002266@users.noreply.github.com>
2023-10-30 09:19:36 -0400

    23

--- Portfolio/js-type work/js loops/jsloop3.js
+++ Portfolio/js-type work/js loops/jsloop3.js
@@ -1,7 +1,7 @@
 // Susan is hired for a job, and her employer agrees to pay her every day.Her employer also agrees that Susan’s salary is 1 penny on the first day, 2 pennies on the second day, and 4 pennies on the third day, continuing to double each day.Create a program that allows the user to enter the number of days that Susan will work and calculates the total amount of pay she will receive over that period of time.
 // the value must start as 1 then multiply by 2 and add the previous value.
 function calculatePay() {
-    let dayLimit = parseInt(document.getElementById("days").value) * 1;
+    let dayLimit = document.getElementById("days").value * 1;
     let startingPay = 1;
 
     for (let i = 0; i < dayLimit; i++) {
---
+++ Portfolio/js-type work/js loops/jsloop4.js
@@ -0,0 +1,5 @@
+function calculateRise {
+    let a = document.getElementById("yearGrow")
+    let b = 15
+    c = a * b
+}
