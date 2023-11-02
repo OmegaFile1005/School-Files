9157f498d7b9b9d0733d076f7b78c46d8a8cf966
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-02 07:44:30 -0400

    x

--- Portfolio/js-type work/js arrays/jsarray3.js
+++ Portfolio/js-type work/js arrays/jsarray3.js
@@ -1,12 +1,22 @@
 function findAccount() {
-    let numbers = Math.floor(Math.random() * 9999999) + 1000000;
-    
-    console.log(numbers);
+    const accountNumbers = [];
 
-    while (numbers.length < 7) {
-        numbers = '0' + numbers;
+    for (let i = 0; i < 5; i++) {
+        const randomNumber = Math.floor(Math.random() * 9999999) + 1000000;
+        accountNumbers.push(randomNumber);
     }
 
-    const randomElement = document.getElementById('random');
-    randomElement.innerHTML = `Account Numbers Available: ${numbers}`;
+    const account = document.getElementById('random').value;
+
+    console.log(accountNumbers);
+
+    document.getElementById('account').value = '';
+
+    account.innerHTML = `Account Numbers Available: ${accountNumbers.join(' | ')}<br>`;
+
+    if (accountNumbers.includes(account)) {
+        alert(`The account number ${account} is available!`);
+    } else {
+        alert(`The account number ${account} is not available!`);
+    }
 }
