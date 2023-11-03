bb8e0cf87406ee5d354c8ba1f806259ab3dc5ab4
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-03 10:38:42 -0400

    f

--- Portfolio/js-type work/js arrays/jsarray6.js
+++ Portfolio/js-type work/js arrays/jsarray6.js
@@ -25,19 +25,59 @@ function updateFruitList() {
 }
 
 function addAtBeginning() {
+    var input = document.getElementById('fruit');
+    var fruit = input.value.trim();
+
+    if (fruit) {
+        fruits.unshift(fruit);
+        input.value = '';
+    }
+    updateFruitList();
 }
 
 function addAtEnd() {
+    var input = document.getElementById('fruit');
+    var fruit = input.value.trim();
+
+    if (fruit) {
+        fruits.push(fruit);
+        input.value = '';
+    }
+    updateFruitList();
 }
 
 function removeFirst() {
+    fruits.shift();
+    updateFruitList();
 }
 
 function removeAtPosition() {
+    var input = document.getElementById('position');
+    var position = parseInt(input);
+    var positionInput = document.getElementById('position');
+    var position = parseInt(positionInput?.value || 0);
+
+    if (isNaN(position) || position < 0 || position >= fruits.length) return;
+
+    fruits.splice(position, 1);
+    positionInput = '';
+    updateFruitList();
 }
 
+
 function addAtPosition() {
+    var input = document.getElementById('fruit');
+    var positionInput = document.getElementById('position');
+    var position = parseInt(positionInput ? positionInput.value : 0);
+    var fruit = input.value;
+
+    if (isNaN(position) || position < 0 || position > fruits.length) return;
+
+    fruits.splice(position, 0, fruit);
+    updateFruitList();
 }
 
 function sortFruits() {
+    fruits.sort();
+    updateFruitList();
 }
