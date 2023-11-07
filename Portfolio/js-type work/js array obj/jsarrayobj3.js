0c9b0449e5472d8beafed2fe7887a974f21bc784
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-07 13:43:46 -0500

    42

---
+++ Portfolio/js-type work/js array obj/jsarrayobj3.js
@@ -0,0 +1,89 @@
+function reserveAirplane() {
+    const selectedSeats = [];
+    const firstClassRadio = document.getElementById('first class');
+    const economicRadio = document.getElementById('economic');
+    const checkboxes = document.querySelectorAll('#plane input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedSeats.push(checkbox.id);
+            checkbox.disabled = true;
+        }
+    });
+
+    let ticketPrice = firstClassRadio.checked ? 295 :
+        economicRadio.checked ? 230 : 0;
+
+    localStorage.setItem('selectedSeats', JSON.stringify(selectedSeats));
+    localStorage.setItem('ticketPrice', ticketPrice);
+
+    console.log('Airplane reserved');
+}
+
+function reserveHotel() {
+    const selectedRooms = Array.from(document.querySelectorAll('#hotel input[type="checkbox"]'))
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.id);
+
+    document.querySelectorAll('#hotel input[type="checkbox"]')
+        .forEach(checkbox => checkbox.checked && (checkbox.disabled = true));
+
+    // Add the readonly attribute to the radio inputs
+    document.querySelectorAll('#hotel input[type="radio"]')
+        .forEach(radio => radio.readOnly = true);
+
+    const roomPrice = getRoomPrice();
+
+    localStorage.setItem('selectedRooms', JSON.stringify(selectedRooms));
+    localStorage.setItem('roomPrice', roomPrice);
+
+    console.log('Hotel reserved');
+}
+
+function getRoomPrice() {
+    const penthouseRadio = document.getElementById('penthouse');
+    const regularRadio = document.getElementById('regular');
+
+    return penthouseRadio.checked ? 180 : regularRadio.checked ? 120 : 0;
+}
+
+function showReservations() {
+    const planeReservations = JSON.parse(localStorage.getItem('selectedSeats')) || [];
+    const hotelReservations = JSON.parse(localStorage.getItem('selectedRooms')) || [];
+
+    if (planeReservations.length > 0 && hotelReservations.length > 0) {
+        const planeReservationsTable = createTable(planeReservations, ['table', 'table-striped']);
+        const hotelReservationsTable = createTable(hotelReservations, ['table', 'table-striped', 'table-dark']);
+
+        const planeReservationsContainer = document.getElementById('planeReservations');
+        if (planeReservationsContainer) {
+            planeReservationsContainer.innerHTML = '';
+            planeReservationsContainer.appendChild(planeReservationsTable);
+        }
+
+        const hotelReservationsContainer = document.getElementById('hotelReservations');
+        if (hotelReservationsContainer) {
+            hotelReservationsContainer.innerHTML = '';
+            hotelReservationsContainer.appendChild(hotelReservationsTable);
+        }
+
+        console.log('Showing reservations');
+    } else {
+        console.log('No reservations found');
+    }
+}
+
+function createTable(reservations, classes) {
+    const table = document.createElement('table');
+    table.classList.add(...classes);
+    const tbody = document.createElement('tbody');
+    reservations.forEach(function (reservation) {
+        const row = document.createElement('tr');
+        const cell = document.createElement('td');
+        cell.textContent = reservation;
+        row.appendChild(cell);
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+    return table;
+}
