b5eaa4fb406be94f5d95a5df65f709f3f1f29625
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-17 09:16:53 -0500

    123

--- Portfolio/js-type work/js array obj/jsarrayobj6.js
+++ Portfolio/js-type work/js array obj/jsarrayobj6.js
@@ -1,91 +1,51 @@
+const bookInventory = [];
 function addBook() {
-    const inventory = JSON.parse(localStorage.getItem("books")) || [];
-    var booktitle = document.getElementById("title").value;
-    var identifier = document.getElementById("ISBN").value;
-    var pages = document.getElementById("pagecount").value;
-    var book = {
+    const booktitle = document.getElementById("title").value;
+    const identifier = document.getElementById("ISBN").value;
+    const pages = document.getElementById("pagecount").value;
+    const book = {
         title: booktitle,
         ISBN: identifier,
         pagecount: pages,
         bookavailable: true
     };
-    inventory.push(book);
-    localStorage.setItem("book", JSON.stringify(inventory));
-    console.log(inventory);
+    bookInventory.push(book);
+    localStorage.setItem("book", JSON.stringify(bookInventory));
+    console.log(bookInventory);
 }
 
 function submitBook() {
-    // const bookStatus = document.querySelector("input[name=searchType]:checked").value;
+    const bookInventory = JSON.parse(localStorage.getItem("book") || "[]");
+    const searchBookInput = document.getElementById("searchBook").value;
+    const bookavailable = document.querySelector('input[name=searchType]:checked').value;
+    const bookUpdate = document.querySelectorAll('#searchType input[name=searchType]:checked').value;
 
-    // if (bookStatus === "checkOut") {
-    //     document.getElementById("checkOut").checked = true;
-    // } else {
-    //     document.getElementById("return").checked = true;
-    // }
 
-    // const tbody = document.querySelector("tbody");
-    // const tr = document.createElement("tr");
-    // tbody.appendChild(tr);
-
-    // const tdTitle = document.createElement("td");
-    // const tdISBN = document.createElement("td");
-    // const tdPagecount = document.createElement("td");
-    // const status = document.createElement("td");
-
-    // tr.appendChild(tdTitle);
-    // tr.appendChild(tdISBN);
-    // tr.appendChild(tdPagecount);
-    // tr.appendChild(status);
-
-    // tdTitle.textContent = document.getElementById("title").value;
-    // tdISBN.textContent = document.getElementById("ISBN").value;
-    // tdPagecount.textContent = document.getElementById("pagecount").value;
-
-    // if (bookStatus === "checkOut") {
-    //     status.textContent = "Checked Out";
-    // } else {
-    //     status.textContent = "Available";
-    // }
-
-    // localStorage.setItem("book", JSON.stringify(tdTitle.textContent));
-
-    let bookInventory = document.getElementById('bookList');
-
-    if (localStorage.getItem("book") === null) {
-        bookInventory = [];
-    } else {
-        bookInventory = JSON.parse(localStorage.getItem("book"));
-    }
-
-    localStorage.getItem("book");
-    if (localStorage.getItem("book.ISBN") === document.getElementById("searchBook").value) {
+    if (bookInventory.some(book => book.title === searchBookInput)) {
         console.log("book exists");
-        document.querySelectorAll("searchType").checked = true;
+        document.querySelector("input[name=searchType]").checked = true;
     } else {
         console.log("book does not exist");
-        document.querySelectorAll("searchType").checked = false;
+        document.querySelectorAll("input[name=searchType]").checked = false;
         document.getElementById("searchBook").value = "";
         return;
     }
 
-    if (document.getElementById("checkOut").checked) {
-        document.createAttribute("status").value = "Checked Out";
-    } else {
-        document.createAttribute("status").value = "Available";
+    let bookAvailability = bookavailable === 'Return' ? 'Available' :
+        bookavailable === 'Check Out' ? 'Checked Out' : '';
+
+    const statusAttribute = document.createAttribute("status");
+    statusAttribute.value = bookUpdate ? "Checked Out" : "Available";
+    if (returnChecked) {
+        statusAttribute.value = bookUpdate ? "Available" : "Checked Out";
     }
 
-    if (document.getElementById("return").checked) {
-        document.createAttribute("status").value = "Available";
-    } else {
-        document.createAttribute("status").value = "Checked Out";
-    }
-
-    if (document.getElementById("checkOut").checked || document.getElementById("return").checked) {
+    if (bookUpdate || bookAvailability === 'Available') {
         alert("Book Checked Out or Returned");
     }
 
     document.getElementById("bookList").value = "";
-    document.innerHTML = "<h3>Book List</h3>";
+    document.getElementById("bookList").innerHTML = "<h3>Book List</h3>";
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
@@ -110,4 +70,26 @@ function submitBook() {
     tr.appendChild(thStatus);
 
     thead.appendChild(tr);
+
+    bookInventory.forEach(book => {
+        const tr = document.createElement('tr');
+        const tdTitle = document.createElement('td');
+        const tdISBN = document.createElement('td');
+        const tdPagecount = document.createElement('td');
+        const tdStatus = document.createElement('td');
+
+        tdTitle.textContent = book.title;
+        tdISBN.textContent = book.ISBN;
+        tdPagecount.textContent = book.pagecount;
+        tdStatus.textContent = book.status;
+
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdISBN);
+        tr.appendChild(tdPagecount);
+        tr.appendChild(tdStatus);
+
+        tbody.appendChild(tr);
+    });
+
+    document.body.appendChild(table);
 }
