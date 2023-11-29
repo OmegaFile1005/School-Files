b227b04a9c835e5d79628f9f4904702f21146d79
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-29 13:41:50 -0500

    234

--- Portfolio/js-type work/js array obj/jsarrayobj6.js
+++ Portfolio/js-type work/js array obj/jsarrayobj6.js
@@ -1,5 +1,7 @@
+    window.onload = showAll();
+
 function addBook() {
-    const bookInventory = JSON.parse(localStorage.getItem("book") || "[]");
+    const bookInventory = JSON.parse(localStorage.getItem("Book")) || [];
     const bookTitle = document.getElementById("title").value;
     const identifier = document.getElementById("ISBN").value;
     const pages = document.getElementById("pagecount").value;
@@ -9,7 +11,7 @@ function addBook() {
         return;
     }
 
-    const bookExists = bookInventory.some(book => book.SerialNumber === Number(identifier));
+    const bookExists = bookInventory.some(book => book.SerialNumber === Number(identifier) && book.title === bookTitle);
     if (bookExists) {
         console.log('Book already exists');
         return;
@@ -19,16 +21,8 @@ function addBook() {
         title: bookTitle,
         SerialNumber: Number(identifier),
         pagecount: Number(pages),
-        bookavailable: false === 'Checked Out',
+        bookavailable: 'Returned',
     };
-
-    if (book.bookavailable !== true) {
-        console.log('Book is not available');
-    } else {
-        console.log('Book is available');
-        book.bookavailable = true;
-        return;
-    }
 
     bookInventory.push(book);
     console.log(bookInventory);
@@ -39,66 +33,50 @@ function addBook() {
 let table = document.querySelector("table");
 function showAll() {
     const allBooks = JSON.parse(localStorage.getItem("Book") || "[]");
-    const bookInventoryList = document.getElementById("bookList");
 
-    bookInventoryList.innerHTML = "<h3>List of Checked Out Books</h3>";
-    let table = document.getElementById("bookList");
-    if (!table) {
-        table = document.createElement('table');
-        table.classList.add('table', 'table-striped');
-        bookInventoryList.appendChild(table);
-    } else {
-        table.innerHTML = '';
-        return;
-    }
-
-function createTableCell(text) {
-    const td = document.createElement('td');
-    td.textContent = text;
-    return td;
-}
-
-function createTableHeader() {
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
-    tr.appendChild(createTableCell('Title'));
-    tr.appendChild(createTableCell('ISBN'));
-    tr.appendChild(createTableCell('Pages'));
-    tr.appendChild(createTableCell('Status'));
-    thead.appendChild(tr);
-    return thead;
-}
-
-function createTableRow(book) {
-    const tr = document.createElement('tr');
-    tr.appendChild(createTableCell(book.title));
-    tr.appendChild(createTableCell(book.SerialNumber));
-    tr.appendChild(createTableCell(book.pagecount));
-    tr.appendChild(createTableCell("Checked Out"));
-    return tr;
-}
-    console.log("Creating table header");
-    table.appendChild(createTableHeader());
-    console.log("Creating table body");
-    table.appendChild(createTableRow(book));
-    console.log("Table created");
+    let list = '';
+    allBooks.forEach(book => {
+        list += `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.SerialNumber}</td>
+            <td>${book.pagecount}</td>
+            <td>${book.bookavailable}</td>
+        </tr>`;
+    })
+    document.getElementById("bookList").innerHTML = `<h3>List of Checked Out Books</h3>
+    <table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>ISBN</th>
+            <th>Pages</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        ${list}
+    </tbody>
+    </table>`;
 }
 
 function submitBook() {
-    const searchedBook = JSON.parse(localStorage.getItem("Book") || "[]");
+    const searchedBook = JSON.parse(localStorage.getItem("Book"));
     const ISBNTitle = document.getElementById("searchBook").value;
-    const bookUpdate = document.querySelector('#searchType input[name=searchType]:checked');
-    const bookExists = searchedBook.some(book => book.SerialNumber === Number(ISBNTitle));
+    const bookUpdate = document.querySelector('input[name=searchType]:checked');
+    const bookExists = searchedBook.find(book => book.SerialNumber === Number(ISBNTitle));
+    console.log(bookExists);
 
-    const bookAvailablity = bookUpdate.value; 
-    if (bookAvailablity === 'available') {
-        bookUpdate.value = 'checked out';
-    } else if (bookAvailablity === 'checked out') {
-        bookUpdate.value = 'available';
+    const bookAvailablity = bookUpdate.value;
+    if (bookAvailablity === 'Returned') {
+        bookUpdate.value = 'Checked Out';
+    } else if (bookAvailablity === 'Checked Out') {
+        bookUpdate.value = 'Returned';
     }
 
     if (ISBNTitle === '') {
         console.log('Invalid input');
+        alert('Invalid input');
         return;
     }
 
@@ -111,4 +89,6 @@ function submitBook() {
         console.log('Book not found');
         return;
     }
+
+    showAll();
 }
