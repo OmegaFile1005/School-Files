343775b6916353daf5ab7218456c326a30a55a31
YongBo <54255241+robert002266@users.noreply.github.com>
2023-11-30 12:29:05 -0500

    vv

---
+++ Portfolio/js-type work/js strings/jsstring1.js
@@ -0,0 +1,64 @@
+function javaString() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function separateCharacters() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function wordCount() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function alphabetDigitsSpecial() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function VowelsConsonants() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function asterisks() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function stringReverse() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function checkParenthesis() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function countOccurences() {
+    let name = "John Doe";
+    let age = 25;
+}
+
+function extractSubstring() {
+
+}
+
+function checkSubstring() {
+
+}
+
+function switchUpperLower() {
+
+}
+
+function positionSubstring() {
+
+}
+
+function compareStrings() {
+
+}
