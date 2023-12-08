1ee02f1357075aee8460068102150dabcf05343e
YongBo <54255241+robert002266@users.noreply.github.com>
2023-12-08 12:42:21 -0500

    d

--- Portfolio/js-type work/js manager/jstaskmanager1.js
+++ Portfolio/js-type work/js manager/jstaskmanager1.js
@@ -21,27 +21,19 @@ function register() {
         loggedIn: 'Inactive',
     };
 
-    // Check if the user already exists
-    var storedUser = localStorage.getItem("user");
-
-    if (storedUser) {
-        var existingUser = JSON.parse(storedUser);
-
-        if (existingUser.username === user.username) {
-            alert("User already exists. Please log in.");
-            return;
-
-        } else {
-            // Add the new user to the existing users
-            var users = JSON.parse(localStorage.getItem("users")) || [];
-            users.push(user);
-            localStorage.setItem("users", JSON.stringify(users));
-        }
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let existingUser = users.find((u) => u.username === user.username);
+    if (existingUser) {
+        alert("User already exists. Please log in.");
+        return;
+    } else {
+        // Add the new user to the existing users
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        // Display a success message or redirect to another page
+        alert("User registered successfully!");
+        window.location.href = "index.html";
     }
-
-    // Display a success message or redirect to another page
-    alert("User registered successfully!");
-    window.location.href = "index.html";
 }
 
 function createAccount() {
@@ -59,41 +51,36 @@ function getUserData() {
 }
 
 function logIn() {
-    const storedUser = localStorage.getItem("user");
-
-    if (!storedUser) {
-        alert("No user found. Please register first.");
-        return;
-    }
+    let users = JSON.parse(localStorage.getItem("users"));
 
     const loginUsername = document.getElementById("userName").value;
     const loginPassword = document.getElementById("pass").value;
 
-    const user = JSON.parse(storedUser);
+    const user = users.find((u) => u.username === loginUsername && u.password === loginPassword);
 
-    if (loginUsername === user.username && loginPassword === user.password) {
-        alert("Logged in successfully!");
-
-        const loggedUser = JSON.parse(localStorage.getItem('user'));
-
-        loggedUser.loggedIn = 'Active';
-
-        localStorage.setItem('user', JSON.stringify(loggedUser));
-
-        window.location.href = "tasks.html";
-
-        const activeUser = document.getElementById('activeUser');
-        activeUser.textContent = loggedUser.fullName;
-    } else {
+    if (!user) {
         alert("Invalid username or password.");
         return;
     }
+
+    if (user.loggedIn === "Active") {
+        alert("You are already logged in.");
+        return;
+    }
+
+    user.loggedIn = 'Active';
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Logged in successfully!");
+
+    window.location.href = "tasks.html";
 }
 
 function logOut() {
     // Remove the logged-in user's status and remove it from loggedInUsers array
 
     const loggedUser = JSON.parse(localStorage.getItem('user'));
+    console.log(loggedUser);
 
     // Update the logged-in user's status
     loggedUser.loggedIn = 'Inactive';
@@ -106,29 +93,38 @@ function logOut() {
 
 function addTask() {
     const taskInput = document.getElementById('task');
-    const task = taskInput.value;
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || {};
+    let user = JSON.parse(localStorage.getItem('user'));
+    const task = { // Task must be an object with a title property and a completed property
+        title: taskInput.value,
+        completed: false,
+    };
 
-    if (!task || task === '') {
+    if (!task.title || task.title === '') {
         taskInput.classList.add('is-invalid');
         return;
     } else {
         taskInput.classList.remove('is-invalid');
     }
 
+    if (user.loggedIn === 'Inactive') {
+        alert('Please log in to add tasks.');
+        return;
+    }
+
+    // Check if the user already has tasks
+    if (!tasks[user.username]) {
+        tasks[user.username] = []; // Create an empty array for the user if it doesn't exist
+    }
+    let existingTask = tasks[user.username].find((t) => t.title === task.title); // Check if the task already exists
+    if (existingTask) {
+        alert('Task already exists.');
+        return;
+    }
+
     // Task must be added to local storage as a string
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks.push(task);
+    tasks[user.username].push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // Task must be added to the DOM as a tbody element to be selectable and as striped table rows
-    const taskTable = document.getElementById('taskList');
-    const taskRow = document.createElement('tr');
-    taskRow.classList.add('table-striped');
-    taskRow.innerHTML = `
-        <td>${task}</td>
-    `;
-    taskTable.appendChild(taskRow);
-
 
     taskInput.value = '';
 
@@ -137,14 +133,28 @@ function addTask() {
 
 function updateTask() {
     const task = document.getElementById('task').value;
-    //when element is selected, replace button with a selectable icon and vice versa
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    // when a task has been selected via mouse or checkmark icon, the icon must be changed to this icon <i class="bi bi-trash"></i>
 
     if (!task || task === '') {
-        alert('Please enter a task');
+        alert('Please enter a task.');
         return;
     }
 
-    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user.loggedIn === 'Inactive') {
+        alert('Please log in to update tasks.');
+        return;
+    }
+
+    // Check if the user already has tasks
+    if (!tasks[user.username]) {
+        tasks[user.username] = []; // Create an empty array for the user if it doesn't exist
+    }
+    
+
 
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i] === task) {
@@ -152,6 +162,8 @@ function updateTask() {
             break;
         }
     }
+    // when task is completed this icon must be changed to this icon <i class="bi bi-trash"></i>
+    // when task is still incomplete this icon must be changed to this icon <i class="bi bi-check"></i>
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 
@@ -159,37 +171,43 @@ function updateTask() {
 }
 
 function showAllTasks() {
-    //displays all tasks
-
-    const tasks = document.getElementsByClassName('task');
-    for (let i = 0; i < tasks.length; i++) {
-        tasks[i].classList.add('selected');
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let user = JSON.parse(localStorage.getItem('user'));
+    let taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
+    if (tasks && tasks[user.username]) {
+        tasks[user.username].forEach((task) => {
+            const taskRow = document.createElement('tr');
+            taskRow.classList.add('task');
+            taskList.classList.add('table-striped');
+            taskRow.innerHTML = `
+            <td class="text-center"><i class="bi bi-journal-bookmark"></i></td>
+                <td>${task.title}</td>
+                <td class="text-center"><i class="bi bi-check" onclick="updateTask('${task.title}')"></i></td>
+            `;
+            taskList.appendChild(taskRow);
+        });
     }
 }
 
 function showCompletedTasks() {
     const tasks = document.getElementsByClassName('task');
+    // it must filter the tasks that are completed
+    
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i].classList.contains('completed')) {
             tasks[i].classList.add('selected');
         }
     }
+
 }
 
 function showIncompleteTasks() {
     const tasks = document.getElementsByClassName('task');
-    for (let i = 0; i < tasks.length; i++) {
-        if (!tasks[i].classList.contains('completed')) {
-            tasks[i].classList.add('selected');
-        }
-    }
+    // it must filter the tasks that are not completed
 }
 
 function clearCompletedTasks() {
     const tasks = document.getElementsByClassName('task');
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].classList.contains('completed')) {
-            tasks[i].classList.remove('completed');
-        }
-    }
+    // it must remove all the completed tasks
 }
