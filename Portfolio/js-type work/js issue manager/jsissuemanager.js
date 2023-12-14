559872fd422d156c7993604cb79f76fe58c9772b
YongBo <54255241+robert002266@users.noreply.github.com>
2023-12-14 07:39:45 -0500

    dcgh

---
+++ Portfolio/js-type work/js issue manager/jsissuemanager.js
@@ -0,0 +1,110 @@
+function register() {
+    var fullName = document.getElementById("fullname").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Capitalize all words in the full name
+    fullName = fullName.toLowerCase().replace(/(?:^|\s|-)\S/g, function (match) {
+        return match.toUpperCase();
+    });
+
+    if (fullName === "" || username === "" || password === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    // Create a user object
+    var user = {
+        fullName,
+        username,
+        password,
+        loggedIn: 'Inactive',
+    };
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let existingUser = users.find((u) => u.username === user.username);
+    if (existingUser) {
+        alert("User already exists. Please log in.");
+        return;
+    } else {
+        // Add the new user to the existing users
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        // Display a success message or redirect to another page
+        alert("User registered successfully!");
+        window.location.href = "index.html";
+    }
+}
+
+function createAccount() {
+    window.location.href = "register.html";
+}
+
+function getUserData() {
+    var storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+        return JSON.parse(storedUser);
+    }
+
+    return null;
+}
+
+function logIn() {
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    const loginUsername = document.getElementById("userName").value.trim();
+    const loginPassword = document.getElementById("pass").value.trim();
+
+    const user = users.find((u) => u.username === loginUsername && u.password === loginPassword);
+
+    if (!user) {
+        alert("Invalid username or password.");
+        return;
+    }
+
+    if (user.loggedIn === "Active") {
+        alert("You are already logged in.");
+        return;
+    }
+
+    user.loggedIn = 'Active';
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Logged in successfully!");
+
+    window.location.href = "tasks.html";
+}
+
+function logOut() {
+    // Remove the logged-in user's status and remove it from loggedInUsers array
+
+    const loggedUser = JSON.parse(localStorage.getItem('user'));
+    console.log(loggedUser);
+
+    // Update the logged-in user's status
+    loggedUser.loggedIn = 'Inactive';
+
+    // Save the updated user data to local storage
+    localStorage.setItem('user', JSON.stringify(loggedUser));
+
+    window.location.href = "index.html";
+}
+
+function addIssue() {
+    
+}
+
+function closeIssue(index) {
+    // Update the code below to be compatible with your issue tracker system
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const user = JSON.parse(localStorage.getItem('user'));
+    const task = tasks[user.username][index];
+    task.completed = !task.completed;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    showAllTasks();
+}
+
+function deleteIssue(index) {
+    // When deleting an issue, it first needs to be closed in order to be deleted
+}
