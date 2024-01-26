3a1d487526f74a509b72f830be2ccca44a539f0e
Aylin Osorio <aylinosorio260@gmail.com>
2024-01-26 13:46:18 -0500

    asd

--- Portfolio/js-type work/js weight tracker/jsweighttracker.js
+++ Portfolio/js-type work/js weight tracker/jsweighttracker.js
@@ -2,3 +2,20 @@ function registerWeight() {
     window.location.href = "register.html";
 }
 
+function getUserData() {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+}
+
+function validatePhone() {
+    const phone = document.getElementById("phone") * 1;
+    if (phone.length < 10) {
+        alert("Phone number must be at least 10 digits long.")
+        return false;
+    }
+    if (phone.length > 10) {
+        alert("Phone number must be at most 10 digits long.")
+        return false;
+    }
+    return true;
+}
