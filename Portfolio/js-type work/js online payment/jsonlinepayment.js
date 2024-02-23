c6aa9f9fe7d7d259abdb1ca5c82bbb402e6c49b8
Aylin Osorio <aylinosorio260@gmail.com>
2024-02-23 10:15:21 -0500

    x

--- Portfolio/js-type work/js online payment/jsonlinepayment.js
+++ Portfolio/js-type work/js online payment/jsonlinepayment.js
@@ -47,11 +47,5 @@ function getStoredUser() {
 }
 
 function loggingRegisterModal() {
-    const user = getStoredUser();
-    if (user) {
-        document.getElementById("username").value = user.username;
-        document.getElementById("email").value = user.email;
-        document.getElementById("phone").value = user.phone;
-        document.getElementById("password").value = user.password;
-    }
+
 }
